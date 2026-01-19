b2bb4243b4011b0c8979e342be2d430d91b5a39f
Anchal Shukla <118104094+ShuklaAnchal@users.noreply.github.com>
2026-01-19 15:50:14 +0530

    url changed

--- src/app/utils/axios.js
+++ src/app/utils/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   //baseURL: "https://dingwanifoods-backned-code.onrender.com/api/v1",
    //baseURL: "http://localhost:8080/api/v1/",
-   baseURL: process.env.Backned_URL,
+   baseURL: process.env.NEXT_PUBLIC_Backned_URL,
   withCredentials: true,
 });
 
