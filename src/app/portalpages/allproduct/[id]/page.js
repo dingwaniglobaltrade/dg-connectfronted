91c62bcf4f93248250689b5ee57caa9b06712be2
Anchal Shukla <118104094+ShuklaAnchal@users.noreply.github.com>
2026-02-04 13:02:11 +0530

    glb modle

--- src/app/portalpages/allproduct/[id]/page.js
+++ src/app/portalpages/allproduct/[id]/page.js
@@ -46,13 +46,11 @@ const Page = ({ params }) => {
   const media = productData?.media || [];
   const modelFile = media.find((m) => m.type === "GLB");
   const imageFiles = media.filter((m) => m.type === "IMAGE");
-
-  console.log({modelFile});
-  
+ console.log({modelFile});
+ 
   // ✅ Use the SAME hook for model signing
-  const signedModelUrl = useSignedImage(modelFile?.url);
-  console.log({useSignedImage});
-  
+  const signedModelUrl = useSignedImage(modelFile?.fileName);
+console.log({signedModelUrl});
 
   return (
     <ProtectedRoute>
